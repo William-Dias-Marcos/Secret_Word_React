bede97b7d969806c3b106a9c77dbfc4aaa12cf08
William Dias Marcos <william.diasmarcos@gmail.com>
2022-04-26 10:45:22 -0300

    Part 06

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -1,8 +1,8 @@
 export const wordsList = {
   carro: ["Motor", "Porta", "Farol", "Pneu", "Pisca"],
   fruta: ["Uva", "Kiwi", "Manga", "Pera", "Laranja"],
-  corpo: ["Braço", "Perna", "Dedo", "Canela", "Olho"],
-  roupa: ["Camisa", "Calça", "Casaco", "Bota"],
+  corpo: ["Bariga", "Perna", "Dedo", "Canela", "Olho"],
+  roupa: ["Camisa", "Cinto", "Casaco", "Bota"],
   bebida: ["Suco", "Vinho", "Cerveja", "Chopp"],
   comida: ["Arroz", "Massa", "Milho", "Carne", "Ovo"],
 };
